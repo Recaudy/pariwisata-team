c2bf24b0fd4ade7288e4265b7bbd6c347a0f9361
Evan Mahesa <evanmahesa14@gmail.com>
2025-12-20 14:45:49 +0700

    ini

--- lib/widget/aksi_icon.dart
+++ lib/widget/aksi_icon.dart
@@ -3,7 +3,6 @@ import 'package:lottie/lottie.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-/// ================= OPEN BOTTOM SHEET =================
 void openKomentarSheet(BuildContext context, String wisataId) {
   showModalBottomSheet(
     context: context,
